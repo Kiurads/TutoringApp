9934f11d0631df4958d5dc319dd5abf944c08a46
Guilherme Silva <guicuradocurado@hotmail.com>
2025-02-21 20:24:29 +0000

    Payments working Page needs work On success class needs to be updated

--- app/main/classes/[id]/pay/page.tsx
+++ app/main/classes/[id]/pay/page.tsx
@@ -1,17 +1,36 @@
 import { fetchClassById } from "@/app/lib/actions/classes.actions";
-import PaymentForm from "@/app/ui/payment/payment-form";
+import { stripe } from "@/app/lib/stripe";
+import CheckoutForm from "@/app/ui/payment/checkout-form";
 
-export default async function PayClassPage(props: {
+export default async function ClassPayPage(props: {
 	params: Promise<{ id: string }>;
 }) {
-	const params = await props.params;
-	const classId = params.id;
-	const classData = await fetchClassById(classId);
+	const { id } = await props.params;
+	const classData = await fetchClassById(id);
+
+	if (!classData || !classData.totalPrice) {
+		return <div>Class not found</div>;
+	}
+
+	const classPrice = parseFloat(classData?.totalPrice) * 100;
+
+	// Create PaymentIntent as soon as the page loads
+	const { client_secret: clientSecret } = await stripe.paymentIntents.create({
+		amount: classPrice,
+		currency: "eur",
+		// In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+		automatic_payment_methods: {
+			enabled: true,
+		},
+	});
 
 	return (
-		<div className="flex flex-col items-center w-screen">
-			<h1>Checkout</h1>
-			<PaymentForm />
+		<div className="hero bg-base-100 min-h-screen" id="checkout">
+			<div className="hero-content flex-col lg:flex-row">
+				<div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+					<CheckoutForm clientSecret={clientSecret} />
+				</div>
+			</div>
 		</div>
 	);
 }
