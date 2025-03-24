6bfef3692fff814a05be4788cc3b82e6855903b3
Guilherme Silva <guicuradocurado@hotmail.com>
2025-03-24 11:16:48 +0000

    Transfer

--- app/main/classes/page.tsx
+++ app/main/classes/page.tsx
@@ -15,8 +15,6 @@ export default async function ClassesPage() {
 	const userEmail = session.user.email;
 	const bookedClasses = await fetchBookedClassesByUser(userEmail);
 
-	console.log(bookedClasses);
-
 	return (
 		<div className="flex flex-col md:flex-row w-full px-2 sm:px-2 lg:px-4 py-4 gap-4">
 			<div className="flex-grow">
